bffef4e511151ed303125ebd722ca95aaaea376d
Gabe Harvey <harvey7@gmail.com>
2025-04-14 12:54:43 -0500

    Added Animation

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Flex, Image, Text, VStack, HStack, Icon, Link } from '@chakra-ui/react';
-import { FaEnvelope, FaPhone, FaLinkedin } from 'react-icons/fa';
+import { FaEnvelope, FaPhone } from 'react-icons/fa';
 // eslint-disable-next-line no-unused-vars
 import { useSpring, animated } from '@react-spring/web';
 
@@ -20,6 +20,15 @@ const MainContent = () => {
     flipSound.play();
     setFlipped(!flipped);
   };
+
+  const pulseBounceProps = useSpring({
+    loop: { reverse: true },
+    to: [
+      { transform: 'scale(1.4) translateY(-20px)' },
+      { transform: 'scale(1) translateY(0)' }
+    ],
+    config: { mass: 1, tension: 50, friction: 12 },
+  });
 
   return (
     <Flex
@@ -70,16 +79,27 @@ const MainContent = () => {
               mt={6}
               mb={2}
             />
-            <Text className='card-name' fontSize="3xl" fontWeight="bold" color="beige" fontFamily="'Pacifico', cursive" marginBottom= '15px' sx={{
-                textShadow: `
-                    0px 2px 4px rgba(184, 134, 11, 0.9),
-                    0px 4px 8px rgba(218, 165, 32, 0.8),
-                    0px 6px 12px rgba(255, 215, 0, 0.7),
-                    0px 8px 16px rgba(255, 239, 184, 0.6),
-                    0px 10px 20px rgba(255, 250, 205, 0.5)`,
-            }}>
-              Madison Nunn
-            </Text>            
+            {/* Animated Text */}
+            <animated.div style={pulseBounceProps}>
+              <Text
+                className='card-name'
+                fontSize="3xl"
+                fontWeight="bold"
+                color="beige"
+                fontFamily="'Pacifico', cursive"
+                marginBottom='15px'
+                sx={{
+                  textShadow: `
+                      0px 2px 4px rgba(184, 134, 11, 0.9),
+                      0px 4px 8px rgba(218, 165, 32, 0.8),
+                      0px 6px 12px rgba(255, 215, 0, 0.7),
+                      0px 8px 16px rgba(255, 239, 184, 0.6),
+                      0px 10px 20px rgba(255, 250, 205, 0.5)`
+                }}
+              >
+                Madison Nunn
+              </Text>
+            </animated.div>
             <Text fontSize="md" color="beige" mb={4} fontFamily="'Pathway Extreme', sans-serif" fontWeight='bold'>
               Director of Women's Sports
             </Text>
@@ -92,26 +112,22 @@ const MainContent = () => {
                 <Icon as={FaPhone} color='green.300'/>
                 <Text color='beige' fontWeight='bold'>(210) 478-6918</Text>
               </HStack>
-              {/* <HStack>
-                <Icon as={FaLinkedin} color='royalblue' />
-                <Link href="https://www.linkedin.com/in/gabe-harvey-37ab90308/" isExternal color='beige'>linkedin.com/in/gabeharvey</Link>
-              </HStack> */}
             </VStack>
             <Link href="https://www.athletexelite.com" isExternal _hover={{ textDecoration: 'none' }}>
-            <Text 
-                fontSize="2xl" 
-                fontWeight="bold" 
-                color="beige" 
-                mb={4} 
-                mt={4} 
+              <Text
+                fontSize="2xl"
+                fontWeight="bold"
+                color="beige"
+                mb={4}
+                mt={4}
                 fontFamily="'Tilt Prism', sans-serif"
                 _hover={{
-                transform: 'scale(1.1)', 
-                transition: 'transform 0.3s ease', 
+                  transform: 'scale(1.1)',
+                  transition: 'transform 0.3s ease',
                 }}
-            >
+              >
                 Athlete X Elite
-            </Text>
+              </Text>
             </Link>
           </Flex>
         </animated.div>
@@ -144,7 +160,7 @@ const MainContent = () => {
                 0px 4px 8px rgba(218, 165, 32, 0.8),
                 0px 6px 12px rgba(255, 215, 0, 0.7),
                 0px 8px 16px rgba(255, 239, 184, 0.6),
-                0px 10px 20px rgba(255, 250, 205, 0.5)`,
+                0px 10px 20px rgba(255, 250, 205, 0.5)`
           }}
         >
           <Flex direction="column" align="center" justify="center" height="100%">
